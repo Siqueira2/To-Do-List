1f07ed9b6763421a32a8cd159f409050d96a0ddf
Guilherme Siqueira <107515511+Siqueira2@users.noreply.github.com>
2022-12-29 13:37:44 -0300

    update commit

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const searcInput = document.querySelector("#search")
 const erase = document.querySelector("#erase-btn")
 const filter = document.querySelector("#filter-select")
 const todoList = document.querySelector("#todo-list")
+let oldInputValue
 
 //Functions
 const saveTodo = (text) => {
@@ -43,6 +44,18 @@ const toggleForm = () => {
     editForm.classList.toggle("hide")
     todoList.classList.toggle("hide")
 }
+const updateTodo = (text) => {
+    const todo = document.querySelectorAll(".todo")
+
+    todo.forEach((todo) => {
+        let todoTitle = todo.querySelector("h4")
+        console.log(todo, text)
+        
+        if(todoTitle.innerText === oldInputValue){
+            todoTitle.innerText = text
+        }
+    })
+}
 
 //Events
 todoForm.addEventListener("submit", (e) => {
@@ -58,6 +71,11 @@ todoForm.addEventListener("submit", (e) => {
 document.addEventListener("click", (e) => {
     const targetEl = e.target
     const parentEl = targetEl.closest("div")
+    let todoTitle
+
+    if (parentEl && parentEl.querySelector("h4")){
+        todoTitle = parentEl.querySelector("h4").innerText 
+    }
 
     if (targetEl.classList.contains("finish-todo")) {
         parentEl.classList.toggle("done")
@@ -65,9 +83,29 @@ document.addEventListener("click", (e) => {
     
     if (targetEl.classList.contains("edit-todo")) {
         toggleForm()
+
+        editInput.value = todoTitle
+        oldInputValue = todoTitle
     }
 
     if (targetEl.classList.contains("remove-todo")) {
         parentEl.remove()
     }
+})
+
+cancelbtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    toggleForm()
+})
+
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const editInputValue = editInput.value
+
+    if(editInputValue) {
+        updateTodo(editInputValue)
+    }
+
+    toggleForm()
 })
